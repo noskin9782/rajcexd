3e5a7ec40050eb95da973739a60ee04321be1da9
noskin9782 <47121937+noskin9782@users.noreply.github.com>
2019-11-02 21:41:24 +0100

    Add files via upload

---
+++ soutez.py
@@ -0,0 +1,24 @@
+import requests
+import random
+import string
+import random
+from proxy_requests import ProxyRequests
+
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36 OPR/63.0.3368.71'
+}
+
+while True:
+    #from proxy_requests import ProxyRequests
+    #register_data = {"POST":"/vote/479 HTTP/1.1"}
+    #print (register_data)
+    #url = 'https://souteze.rajce.net/vote/479'
+    #r = s.post(url, json=register_data, headers=headers, proxies = proxies)
+    #print(r.content)
+    r = ProxyRequests("https://souteze.rajce.net/vote/504")
+    r.post({"POST":"/vote/504 HTTP/1.1"})
+    print(r)
+    print(r.get_status_code())
+    print(r.get_proxy_used())
